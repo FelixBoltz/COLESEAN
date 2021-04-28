62d01777d733b1406704feeaaed5873ba5e8e942
Felix Boltz <ucdhl@student.kit.edu>
2021-04-28 16:45:05 +0200

    changed to not defining a regressor class, but just methods instead

--- Regressor.py
+++ Regressor.py
@@ -1,45 +1,26 @@
-# input configuration
-# model
-# WordEmbedding
-# SenticNet Inclusion
-# output configuration
-
 import tensorflow as tf
 from tensorflow import keras
-from keras import callbacks
-from keras.callbacks import EarlyStopping
 from keras import layers
 from keras.layers import LSTM, Bidirectional
 
 
-class Regressor:
-    word_embedding = ''
-    sentic_net = ''
-    model_type = 0
-
-    def __init__(self, word_embedding, sentic_net, model_type):
-        self.word_embedding = word_embedding
-        self.sentic_net = sentic_net
-        self.model_type = model_type
-
-    def train(self, input_data):
-        return 0+input_data
-
-
-def concept_vector_model(maxlen_we, maxlen_as):
+def concept_vector_model(max_len_we, max_len_as, vocab_size_we, vocab_size_as,
+                         embedding_dim_we, embedding_matrix_we,
+                         embedding_dim_as, embedding_matrix_as):
+    # defining keras model
     tf.keras.backend.clear_session()
-    we_input = keras.Input(shape=(maxlen_we,), name="we_sequence")
-    as_input = keras.Input(shape=(maxlen_as,), name="concept_sequence")
-    we_embedding_layer = layers.Embedding(vocab_size_WE, embedding_dim_WE, weights=[embedding_matrix_WE],
-                                          input_length=maxlen_WE, trainable=False)(we_input)
-    as_embedding_layer = layers.Embedding(vocab_size_AS, embedding_dim_AS, weights=[embedding_matrix_AS],
-                                          input_length=maxlen_AS, trainable=False)(as_input)
-    we_BiLSTM = Bidirectional(LSTM(64))(we_embedding_layer)
-    as_BiLSTM = Bidirectional(LSTM(64))(as_embedding_layer)
-    we_dense = layers.Dense(128, activation='relu')(we_BiLSTM)
-    as_dense = layers.Dense(128, activation='relu')(as_BiLSTM)
-    conc = layers.concatenate([we_dense, as_dense])
-    extra_dense = layers.Dense(10, activation='relu')(conc)
+    we_input = keras.Input(shape=(max_len_we,), name="we_sequence")
+    as_input = keras.Input(shape=(max_len_as,), name="concept_sequence")
+    we_embedding_layer = layers.Embedding(vocab_size_we, embedding_dim_we, weights=[embedding_matrix_we],
+                                          input_length=max_len_we, trainable=False)(we_input)
+    as_embedding_layer = layers.Embedding(vocab_size_as, embedding_dim_as, weights=[embedding_matrix_as],
+                                          input_length=max_len_as, trainable=False)(as_input)
+    we_bi_lstm = Bidirectional(LSTM(64))(we_embedding_layer)
+    as_bi_lstm = Bidirectional(LSTM(64))(as_embedding_layer)
+    we_dense = layers.Dense(128, activation='relu')(we_bi_lstm)
+    as_dense = layers.Dense(128, activation='relu')(as_bi_lstm)
+    concat = layers.concatenate([we_dense, as_dense])
+    extra_dense = layers.Dense(10, activation='relu')(concat)
     output = layers.Dense(1)(extra_dense)
     model = keras.Model(inputs=[we_input, as_input], outputs=[output], )
     return model
